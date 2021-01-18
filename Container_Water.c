edcfd5fc4298175eda4da375d99e9c3b634e977f
KaiSWang <31958997+KaiSWang@users.noreply.github.com>
2021-01-18 08:04:54 +0800

    add Container With Most Water

---
+++ Container_Water.c
@@ -0,0 +1,20 @@
+/* Bruce method
+   cannot pass! too slow.
+*/
+
+int maxArea(int* height, int heightSize){
+    int index, rIndex;
+    int maxVol = 0;
+    for (index=0; index<heightSize-1; index++){
+        int dif;
+        //int maxDif = 0;
+        for (rIndex = heightSize-1; rIndex>index; rIndex--){
+            dif = height[index] - height[rIndex];
+            //maxDif = maxDif < diff? diff:maxDif;
+            int vol;
+            vol = dif>=0?height[rIndex] * (rIndex - index):height[index] * (rIndex - index);
+            maxVol = maxVol<vol? vol:maxVol;
+        }
+    }
+    return maxVol;
+}
