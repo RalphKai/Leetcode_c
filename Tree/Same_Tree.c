4819892c0aac4a61ade479273fd283eea854dfc0
KaiSWang <31958997+KaiSWang@users.noreply.github.com>
2021-01-23 17:38:34 +0800

    update same_tree

--- Tree/Same_Tree.c
+++ Tree/Same_Tree.c
@@ -10,8 +10,7 @@
 
 bool isSameTree(struct TreeNode* p, struct TreeNode* q){
     if (!p && !q) return true;
-    if (p && !q) return false;
-    if (!p && q) return false;
+    if (!p || !q) return false;
     if (p->val != q->val) return false;
     
     
